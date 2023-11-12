c0411194159124cc86abe71cb4cf8bf2a6f8a28a
Chris Noreikis <chris.nore@gmail.com>
2023-11-12 15:39:25 -0600

    nextAired -> lastAired

--- src/main.py
+++ src/main.py
@@ -66,30 +66,29 @@ def save_db(last_aired_db):
     db_ptr.put(Body=json.dumps(last_aired_db))
 
 
-def find_diffs(last_aired_db, secrets):
-    tv = tvdb_v4_official.TVDB(secrets['API_KEY'], secrets['PIN'])
+def process_show_updates(db, secrets):
+    tv_client = tvdb_v4_official.TVDB(secrets['API_KEY'], secrets['PIN'])
 
-    for cached_show in last_aired_db['shows']:
-        api_resp = tv.get_series_nextAired(cached_show['id'])
-        next_aired = api_resp['nextAired']
-        if next_aired != cached_show['nextAired']:
-            cached_show['nextAired'] = next_aired
+    for show in db['shows']:
+        updated_show = tv_client.get_series_nextAired(show['id'])
+        next_aired = updated_show['nextAired']
+        if next_aired != "" and next_aired != show['lastAired']:
+            show['lastAired'] = next_aired
 
-            if next_aired != "":
-                send_notification(cached_show['subscribers'], api_resp)
+            send_notification(show['subscribers'], updated_show)
 
-    save_db(last_aired_db)
+    save_db(db)
 
 
-def callable(ev, ctx):
-    find_diffs(
+def lambda_entrypoint(lambda_event, lambda_ctx):
+    process_show_updates(
         get_db(),
         get_secrets()
     )
 
 
 if __name__ == '__main__':
-    find_diffs(
+    process_show_updates(
         get_db(),
         get_secrets()
     )
